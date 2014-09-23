8380e89508be5cdcda3fab98252a8cc9ec062480
kevinkmckeown <kevinkmckeown@lewisu.edu>
2014-09-23 15:08:52 -0500

    Initial Commit
    Has comments, but no functions added

---
+++ TheatreSeating/TheatreSeating/theatre.cpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+	int totalSales = 0;
+	int row = 0;
+	int seat = 0;
+	int seatsSold = 0;
+
+	int seatingChart[15][30];
+	int sales[15];
+
+	void displayMenu();   // Kevin
+	void displaySales();	// Rodger
+	void displayChart();	// Matt
+	void fillArray();		// Matt
+	void promptRowPrices();	// Rodger
+	void promptRowSeat();	// Kevin
+	void getRowPrice();		// Rodger
+	void changeSeatStatus();	// Matt
+
+
+	// Fill the array
+
+
+	// Prompt for row prices
+
+	// Display Menu outside of while and get userChoice
+
+
+	// While(userChoice !=3)
+		
+		// switch
+
+}
